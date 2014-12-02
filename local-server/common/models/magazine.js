1db599bbf1cc4be58fb835b700f95c9ffe40b403
Simon Ho <simon@strongloop.com>
2014-12-02 14:59:20 -0800

    Fix error logging local-server

--- local-server/common/models/magazine.js
+++ local-server/common/models/magazine.js
@@ -4,13 +4,10 @@ module.exports = function(Magazine) {
 
     var coffeeShopService = Magazine.app.dataSources.CoffeeShopService;
 
-    coffeeShopService.find(function(unknown, response, context) {
-      //what is the first arg? its not err because its always null
-      //is the last argument actually context?
-      //response returns error if there is one
-      var error = response.error;
-      if (error) {
-        next('> error: ' + error.stack);
+    coffeeShopService.find(function(err, response, context) {
+      if (err) throw err; //error making request
+      if (response.error) {
+        next('> response error: ' + response.error.stack);
       }
       model.coffeeShops = response;
       console.log('> coffee shops fetched successfully from remote server');
